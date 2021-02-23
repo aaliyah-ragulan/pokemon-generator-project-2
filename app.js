2ed1fae37cf3c74e9b6be44cf7339d22397cf631
ragulan <ragulanravi21@gmail.com>
2021-02-22 20:52:04 -0500

    updated grid layout and favourite button js function

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ let pokemonType = document.querySelector(".pokemonTypeName");
 let pokemonNumber = document.querySelector(".pokemonNumber");
 let pokemonAbilities = document.querySelector(".pokemonAbilities");
 let pokemonMoves = document.querySelector(".pokemonMoves");
-let favouritePokemon = document.querySelector("ul");
+let favouritePokemon = document.querySelector(".favList");
 let generatorButton = document.querySelector(".generatorBtn");
+
+let favButton = document.querySelector(".favBtn");
 
 //initialize preset data
 //apiURL
@@ -33,55 +35,63 @@ pokemonGenerator.randomizePokemon = Math.floor(Math.random() * 100);
 
 // Click generates a random pokemon, information is store in a new variable
 
-generatorButton.addEventListener("click", function () {
+generatorButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("click");
   fetch(`${pokemonGenerator.apiURL}${pokemonGenerator.randomizePokemon}`)
     .then((response) => {
       return response.json();
     })
 
     .then((jsonResponse) => {
-      console.log(jsonResponse);
+      // console.log(jsonResponse);
       pokemonGenerator.pokemonName = jsonResponse.species["name"];
       let name = pokemonGenerator.pokemonName;
-      
-      pokemonName.innerHTML = name
 
-
+      pokemonName.innerHTML = name;
 
       //   let type = pokemonGenerator.console.log(name);
       //pokemonGenerator.pokemonType = jsonResponse.types[0].type.name
       pokemonGenerator.pokemonType = jsonResponse.types[0]["type"]["name"];
       let type = pokemonGenerator.pokemonType;
       //console.log(type)
-      
-      pokemonType.innerHTML = type
-    
+
+      pokemonType.innerHTML = type;
+
       pokemonGenerator.pokemonImage = jsonResponse.sprites["front_default"];
       let image = pokemonGenerator.pokemonImage;
-      let pokeImage = document.createElement('img')
+      let pokeImage = document.createElement("img");
 
-      pokeImage.src = image
+      pokeImage.src = image;
 
-      pokemonImage.append(pokeImage)
-      
+      pokemonImage.append(pokeImage);
+
       //pokemonImage.src = image
 
       pokemonGenerator.pokemonMoves = jsonResponse.moves[0]["move"]["name"];
       let stats = pokemonGenerator.pokemonMoves;
-      
-      pokemonMoves.innerHTML = stats
 
-      pokemonGenerator.pokemonNumber = jsonResponse.order
-      let pokeNumber = pokemonGenerator.pokemonNumber
-      pokemonNumber.innerHTML = pokeNumber
-        
+      pokemonMoves.innerHTML = stats;
+
+      pokemonGenerator.pokemonNumber = jsonResponse.order;
+      let pokeNumber = pokemonGenerator.pokemonNumber;
+      pokemonNumber.innerHTML = pokeNumber;
+
       pokemonGenerator.pokemonAbilities =
-          jsonResponse.abilities[0]["ability"]["name"];
-        let abilities = pokemonGenerator.pokemonAbilities;
-        
-        pokemonAbilities.innerHTML = abilities
+        jsonResponse.abilities[0]["ability"]["name"];
+      let abilities = pokemonGenerator.pokemonAbilities;
+
+      pokemonAbilities.innerHTML = abilities;
+
+      favButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        favouritePokemon.appendChild(document.createTextNode(name));
+        console.log(name);
+      });
     });
 });
+
+//create event listener on save pokemon button (grab api data outputted)
 
 //create event listener on random generator button (grab api data)
 
